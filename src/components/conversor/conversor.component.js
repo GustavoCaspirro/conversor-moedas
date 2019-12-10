c9b09c9fcdaab451649e67c10d36130775753aef
Gustavo Caspirro <gustavo.caspirro-ext@viavarejo.com.br>
2019-12-10 14:43:16 -0300

    adicionando verificação se já foi feito requisição na api

--- src/components/conversor/conversor.component.js
+++ src/components/conversor/conversor.component.js
@@ -1,53 +1,66 @@
 import axios from "axios";
 export default {
-    name: 'Conversor',
-    props: [
-        'moedaA',
-        'moedaB'
-    ],
-    data() {
-        return {
-            moedaA_value: '',
-            moedaB_value: 0,
-            showLoading: false,
-            currencies: {}
-        };
+  name: 'Conversor',
+  props: [
+    'moedaA',
+    'moedaB'
+  ],
+  data() {
+    return {
+      moedaA_value: '',
+      moedaB_value: 0,
+      showLoading: false,
+      currencies: {}
+    };
+  },
+
+  beforeMount() {
+    this.initializeMoedas()
+  },
+
+  methods: {
+    converter(eventButton) {
+      this.showLoading = true;
+      let currenciesLocalStorage = JSON.parse(localStorage.getItem('currencies'));
+      var currentCurrency = eventButton.target.parentElement.childNodes[0].childNodes[1].value.split('-')[0].trim();
+
+      if (currenciesLocalStorage) {
+        this.showLoading = false;
+        let cotacao = (currenciesLocalStorage[currentCurrency].ask).toString().replace(",", ".");
+        this.moedaB_value = (parseFloat(this.moedaA_value.toString().replace(",", ".")) / cotacao).toFixed(2);
+      } else {
+        axios
+          .get(`https://economia.awesomeapi.com.br/all/${currentCurrency}-BRL`)
+          .then(json => {
+            this.showLoading = false;
+            let value = currentCurrency;
+            let cotacao = (json.data[value].ask).toString().replace(",", ".");
+            this.moedaB_value = (parseFloat(this.moedaA_value.toString().replace(",", ".")) / cotacao).toFixed(2);
+          })
+          .catch(error => {
+            this.showLoading = false;
+            console.log('There has been a problem with your fetch operation: ' + error.message);
+          });
+      }
     },
 
-    beforeMount(){
-        this.initializeMoedas()
-    },
+    initializeMoedas() {
+      let currenciesLocalStorage = localStorage.getItem('currencies');
 
-    methods: {
-        converter(eventButton){
-            let currentCurrency = eventButton.target.parentElement.childNodes[0].childNodes[1].value.split('-')[0].trim();
-            this.showLoading = true;
-            axios
-                .get(`https://economia.awesomeapi.com.br/all/${currentCurrency}-BRL`)
-                
-                .then(json => {
-                    this.showLoading = false;
-                    let value = currentCurrency;
-                    let cotacao = (json.data[value].ask).toString().replace(",",".");
-                    this.moedaB_value = (parseFloat(this.moedaA_value.toString().replace(",",".")) / cotacao).toFixed(2);
-                })
-                .catch(error => {
-                    this.showLoading = false;
-                    console.log('There has been a problem with your fetch operation: ' + error.message);
-                });
-        },
-
-        initializeMoedas() {
-            axios
-                .get(`https://economia.awesomeapi.com.br/all`)
-                
-                .then(json => {
-                    this.currencies = json.data;
-                    console.log(this.currencies)
-                })
-                .catch(error => {
-                    console.log('There has been a problem with your fetch operation: ' + error.message);
-                });
-        }
+      if (currenciesLocalStorage) {
+        this.currencies = JSON.parse(currenciesLocalStorage);
+      } else {
+        axios
+          .get(`https://economia.awesomeapi.com.br/all`)
+          .then(json => {
+            this.currencies = json.data;
+            const currenciesStringify = JSON.stringify(json.data);
+            localStorage.setItem('currencies', currenciesStringify);
+          })
+          .catch(error => {
+            console.log('There has been a problem with your fetch operation: ' + error.message);
+          });
+      }
     }
+  }
 }
