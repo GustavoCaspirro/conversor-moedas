2fb5854803d78870fbd7841cee6b54a0834b4613
Gustavo Caspirro <gustavo.caspirro-ext@viavarejo.com.br>
2019-12-10 16:10:48 -0300

    refatoração telas

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <Header />
 
     <section class="conversor-moedas">
-      <div>
+      <div class="wrapper">
         <router-view></router-view>
       </div>
     </section>
@@ -13,41 +13,42 @@
 </template>
 
 <script>
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     Header,
-    Footer,
+    Footer
   }
-}
+};
 </script>
 
 <style>
-  body {
-    margin: 0;
-    padding: 0;
-  }
+body {
+  margin: 0;
+  padding: 0;
+}
 
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-  }
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-  .conversor-moedas {
-    padding: 50px 10px;
-  }
+.conversor-moedas {
+  padding: 50px 10px;
+}
 
-  .title {
-    margin: 0 0 60px;
-  }
+.title {
+  margin: 0 0 60px;
+}
 
-  .wrapper {
-    display: flex;
-  }
+.wrapper {
+  display: flex;
+  max-width: 1000px;
+}
 </style>
